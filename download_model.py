d39d0d2901e82c1aac783dcf6e7c9be570be3b40
ning.huang <ning.huang.ridson@gmail.com>
2026-06-17 14:42:11 +0800

    Refactor code structure for improved readability and maintainability

---
+++ download_model.py
@@ -0,0 +1,7 @@
+from modelscope import snapshot_download
+
+# 下载 bge-m3 向量模型
+snapshot_download('BAAI/bge-m3', local_dir='./models/bge-m3')
+
+# 下载 bge 重排模型
+snapshot_download('BAAI/bge-reranker-v2-m3', local_dir='./models/bge-reranker-v2-m3')
